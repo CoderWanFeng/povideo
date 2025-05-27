017a32aae5d7f136d728b788d9dd09098f8ca602
CoderWanFeng <1957875073@qq.com>
2025-05-27 21:56:00 +0800

    更新微信号

--- tests/test_video.py
+++ tests/test_video.py
@@ -7,7 +7,7 @@ class TestImage(unittest.TestCase):
     def test_wc(self):
         video2mp3(path=r'D:\software\obs\vedio\2-功能介绍.mp4', output_path=r'./map3_path')
     def test_mark(self):
-        mark2video(video_path=r'D:\software\obs\vedio\2024-12-02_22-36-41.mp4', output_path=r'./map3_path')
+        mark2video(video_path=r'D:\BaiduNetdiskDownload\Pandas-玩转-Excel\视频\Pandas VS Excel', output_path=r'./map4_path')
 
     def test_txt2mp3_all(self):
         # 测试全部功能
